c9200e5c7b57963f4c8d93bee3e6b3be75c03c49
Ola Ajibode <ola.ajibode@gmail.com>
2022-12-22 11:35:18 +0000

    feat: Cleanup conmmented code

--- src/main/scala/com/katchfashion/funcx/counters/Counter.scala
+++ src/main/scala/com/katchfashion/funcx/counters/Counter.scala
@@ -7,31 +7,12 @@ case class Counter(count: Int = 0) {
 
   def dec: Counter = copy(count = count - 1)
 
-  def inc(n: Int): Counter = copy(count = count  + n)
-  def dec(n: Int): Counter = copy(count = count  - n)
-    def adjust(adder: Adder): Counter =  copy(adder.add(count))
-  //
-    def adjustSub(adder: Adder): Counter = copy(adder.sub(count))
+  def inc(n: Int): Counter = copy(count = count + n)
+
+  def dec(n: Int): Counter = copy(count = count - n)
+
+  def adjust(adder: Adder): Counter = copy(adder.add(count))
+
+  def adjustSub(adder: Adder): Counter = copy(adder.sub(count))
 }
 
-//object Counter {
-//
-//  def inc: Counter = Counter(count + 1)
-//
-//  def dec: Counter = Counter(count - 1)
-//
-//  def inc(n: Int): Counter = {
-//    if (n <= 0) this
-//    else inc.inc(n - 1)
-//  }
-//
-//  def dec(n: Int): Counter = {
-//    if (n <= 0) this
-//    else dec.dec(n - 1)
-//  }
-
-//  def adjust(adder: Adder): Counter = new Counter(adder.add(count))
-//
-//  def adjustSub(adder: Adder): Counter = new Counter(adder.sub(count))
-
-//}
--- src/main/scala/com/katchfashion/funcx/counters/RunCounters.scala
+++ src/main/scala/com/katchfashion/funcx/counters/RunCounters.scala
@@ -2,13 +2,13 @@ package com.katchfashion.funcx.counters
 
 object RunCounters extends App {
 
-  val counter = new Counter(7)
-  println(counter.inc.count)
-  println(counter.dec.count)
+//  val counter = Counter(7)
+  println(Counter(7).inc.count)
+  println(Counter().inc.count)
 
-  println(counter.inc(3).count)
-  println(counter.dec(9).count)
-  println(counter.adjust(new Adder(4)).count)
-  println(counter.adjustSub(new Adder(2)).count)
+  println(Counter().inc(3).count)
+  println(Counter().dec(6).inc.count)
+  println(Counter().adjust(new Adder(4)).count)
+  println(Counter().adjustSub(new Adder(2)).count)
 
 }
